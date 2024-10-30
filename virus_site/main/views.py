b25544e869ef869ffa17bfa02e6ba23e064e573f
Praneeth <praneethsimon2@gmail.com>
2024-10-30 22:50:51 +0530

    views

--- virus_site/main/views.py
+++ virus_site/main/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
 
 # Create your views here.
+
+from .models import VirusRecord
+
+def virus_list(request):
+    viruses = VirusRecord.objects.all()
+    return render(request, 'ncbi/virus_list.html', {'viruses': viruses})
+
