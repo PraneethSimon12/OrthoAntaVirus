22be93e279afeff421658de35d360ddc42ec2d30
Praneeth <praneethsimon2@gmail.com>
2024-10-30 22:20:58 +0530

    models

--- virus_site/main/models.py
+++ virus_site/main/models.py
@@ -1,3 +1,16 @@
 from django.db import models
 
 # Create your models here.
+
+
+
+class VirusRecord(models.Model):
+    accession = models.CharField(max_length=50)
+    organism_name = models.CharField(max_length=100)
+    genbank_refseq = models.CharField(max_length=50)
+    assembly = models.CharField(max_length=50)
+    submitter = models.CharField(max_length=100)
+    organization = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.organism_name
